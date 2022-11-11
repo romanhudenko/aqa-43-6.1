8e1b8ccd0ec08d220a37e6d18a6881279e5e0aac
Роман Худенко <damage----------inc@yandex.ru>
2022-11-11 13:11:06 +0400

    Fixed tests

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,7 +1,6 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.web.data.DataHelper;
 
